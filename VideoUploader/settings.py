024b8e2ac3d89ac7bf3f9089fe7bd0da503027bd
Pranav kumar <pranav88096@gmail.com>
2024-06-28 14:41:12 +0530

    something changed in them

--- VideoUploader/settings.py
+++ VideoUploader/settings.py
@@ -18,9 +18,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
